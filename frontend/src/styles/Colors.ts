3208e378f18d7d4ac61d71a3d712b9b4277984aa
Omar Ramirez Hernandez <omarramhdz16@gmail.com>
2025-04-08 09:14:33 -0600

    feat: Add app colors

---
+++ frontend/src/styles/Colors.ts
@@ -0,0 +1,11 @@
+const Colors = {
+  black: "#151515",
+  red: "#a63d40",
+  yellow: "#e9b872",
+  green: "#90a959",
+  blue: "#6494aa",
+  backWhite: "#d9d9d9",
+  white: "#ffffff",
+};
+
+export default Colors;
